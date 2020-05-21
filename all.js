005f1e75174291e3aeacf7836a56469bc08e0864
MattW <drflamerock@yahoo.co.uk>
2020-05-21 15:55:28 +0100

    Change game logic to ES6

---
+++ all.js
@@ -0,0 +1,105 @@
+;(function() {
+  
+  var Player = function(game, gameSize) {
+    this.game = game;
+    this.size = {x: 15, y: 15};
+    this.center = {x: gameSize.x / 2, y: gameSize.y - this.size.x};
+    this.keyboarder = new Keyboarder();
+  };
+
+  Player.prototype = {
+    update: function() {
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
+        this.center.x -= 2;
+      } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
+        this.center.x += 2;
+      }
+      if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
+        var bullet = new Bullet ({x: this.center.x, y: this.center.y - this.size.x / 2}, {x: 0, y: -7});
+        this.game.addBody(bullet);
+      }
+    }
+  };
+
+  var Invader = function(game, center) {
+    this.game = game;
+    this.size = {x: 15, y: 15};
+    this.center = center;
+    this.patrolX = 0;
+    this.speedX = 0.3;
+  };
+
+  Invader.prototype = {
+    update: function() {
+      if (this.patrolX < 0 || this.patrolX > 40) {
+        this.speedX = -this.speedX;
+      }
+
+      this.center.x += this.speedX;
+      this.patrolX += this.speedX;
+
+      if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
+        var bullet = new Bullet ({x: this.center.x, y: this.center.y + this.size.x / 2}, {x: Math.random() - 0.5 , y: 2});
+        this.game.addBody(bullet);
+      }
+    }
+  };
+
+  var Bullet = function(center, velocity) {
+    this.size = {x: 3, y: 3};
+    this.center = center;
+    this.velocity = velocity;
+  };
+
+  Bullet.prototype = {
+    update: function() {
+      this.center.x += this.velocity.x;
+      this.center.y += this.velocity.y;
+    }
+  };
+
+
+  var createInvaders = function(game) {
+    var invaders = [];
+    for (var i = 0; i < 24; i++) {
+      var x = 30 + (i % 8) * 30;
+      var y = 30 + (i % 3) * 30;
+      invaders.push(new Invader(game, {x: x, y: y}));
+    }
+    return invaders;
+  };
+
+  var drawRect = function(screen, body) {
+    screen.fillRect(body.center.x - body.size.x / 2,
+      body.center.y - body.size.y / 2,
+      body.size.x, body.size.y);
+    };
+
+    var Keyboarder = function() {
+      var keyState = {};
+
+      window.onkeydown = function(e) {
+        keyState[e.keyCode] = true;
+      };
+      window.onkeyup = function(e) {
+        keyState[e.keyCode] = false;
+      };
+      this.isDown = function(keyCode) {
+        return keyState[keyCode] === true;
+      };
+      this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
+    };
+
+    var colliding = function(b1, b2) {
+      return !(b1 === b2 ||
+        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x /2 ||
+        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y /2 ||
+        b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x /2 ||
+        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y /2);
+      };
+
+      window.onload = function() {
+        let game = new Game('screen');
+        game.run();
+      };
+    })();
