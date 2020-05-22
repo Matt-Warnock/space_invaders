e8b31b66ca6aaa4e3a944173676849de033e00e8
MattW <drflamerock@yahoo.co.uk>
2020-05-22 13:19:57 +0100

    Add player boundary

--- player.js
+++ player.js
@@ -1,6 +1,7 @@
 class Player {
   constructor(game, gameSize) {
     this.game = game;
+    this.canvasLeftSide = 0;
     this.size = {x: 15, y: 15};
     this.center = {x: gameSize.x / 2, y: gameSize.y - this.size.x};
     this.keyboarder = new Keyboarder();
@@ -10,6 +11,11 @@ class Player {
       this.center.x -= 2;
     } else if (this.keyboarder.isDown(this.keyboarder.KEYS.RIGHT)) {
       this.center.x += 2;
+    }
+    if (this.center.x - this.size.x / 2  < this.canvasLeftSide) {
+      this.center.x += 2;
+    }else if (this.center.x + this.size.x / 2 > this.game.canvas.width) {
+      this.center.x -= 2;
     }
     if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
       let bullet = new Bullet ({x: this.center.x, y: this.center.y - this.size.x / 2}, {x: 0, y: -7});
