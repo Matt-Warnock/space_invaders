0e92454341b4217f823d4847d7ec7405f6ef702e
MattW <drflamerock@yahoo.co.uk>
2020-05-23 13:47:58 +0100

    Fix one shot without circle referncing

--- player.js
+++ player.js
@@ -3,9 +3,10 @@ class Player {
     this.game = game;
     this.margin = 2;
     this.size = {x: 15, y: 15};
-    this.center = {x: gameSize.x / this.margin, y: gameSize.y - this.size.x};
+    this.center = {x: gameSize.x / 2, y: gameSize.y - this.size.x};
     this.keyboarder = new Keyboarder();
   }
+
   update() {
     if (this.keyboarder.isDown(this.keyboarder.KEYS.LEFT)) {
       this.center.x -= 2;
@@ -17,10 +18,9 @@ class Player {
     }else if (this.center.x + this.size.x / this.margin >= this.game.gameSize.x) {
       this.center.x -= 2;
     }
-    if (this.keyboarder.isDown(this.keyboarder.KEYS.SPACE)) {
-      let bullet = new Bullet ({x: this.center.x, y: this.center.y - this.size.x / this.margin}, {x: 0, y: -7});
+    if (this.keyboarder.keyTap(this.keyboarder.KEYS.SPACE)) {
+      let bullet = new Bullet ({x: this.center.x, y: this.center.y - this.size.x / 2}, {x: 0, y: -7});
       this.game.addBody(bullet);
-      this.keyboarder.keyState[this.keyboarder.KEYS.SPACE] = false;
     }
   }
 }
