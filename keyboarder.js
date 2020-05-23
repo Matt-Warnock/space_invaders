a001429ddaa9813be65108f8c9980a910a074c3a
MattW <drflamerock@yahoo.co.uk>
2020-05-23 13:47:58 +0100

    Fix one shot without circle referncing

--- keyboarder.js
+++ keyboarder.js
@@ -3,6 +3,7 @@ class Keyboarder {
   constructor() {
     this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
     this.keyState = {};
+    this.isKeyUp = true;
 
     window.onkeydown = (e) => {
       this.keyState[e.keyCode] = true;
@@ -11,8 +12,19 @@ class Keyboarder {
       this.keyState[e.keyCode] = false;
     };
   }
-  
+
   isDown(keyCode) {
     return this.keyState[keyCode] === true;
+  }
+
+  keyTap(keyCode) {
+    if (this.keyState[keyCode] === false) {
+      this.isKeyUp = true;
+    }
+    if (!this.isKeyUp) return;
+    else if (this.keyState[keyCode] === true) {
+      this.isKeyUp = false;
+      return true;
+    }
   }
 }
