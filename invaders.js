95615fc61b1510f48e7e1cf868a2ee91be490cd9
MattW <drflamerock@yahoo.co.uk>
2020-06-09 16:10:38 +0100

    Add invader y movement

--- invaders.js
+++ invaders.js
@@ -4,18 +4,25 @@ class Invader {
     this.size = {x: 15, y: 15};
     this.center = center;
     this.patrolX = 0;
+    this.patrolY = 0;
     this.speedX = 0.3;
+    this.speedY = 7;
   }
 
   update() {
-    if (this.patrolX < 0 || this.patrolX > 40) {
-      this.speedX = -this.speedX;
-    }
-
     this.center.x += this.speedX;
     this.patrolX += this.speedX;
 
     this.game.sound.invaderStep();
+
+    if (this.patrolX < -14 || this.patrolX > 54) {
+      this.speedX = -this.speedX;
+      if (this.patrolY < 0 || this.patrolY > 60) {
+        this.speedY = -this.speedY;
+      }
+      this.center.y += this.speedY;
+      this.patrolY += this.speedY;
+    }
 
     if (Math.random() > 0.995 && !this.invadersBelow(this)) {
       let bullet = new Bullet ({x: this.center.x, y: this.center.y + this.size.x / 2}, {x: Math.random() - 0.5 , y: 2});
